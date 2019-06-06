b9b342355ffeddc941ba4dd1a2612738a8931a2c
13550864646@163.com <13550864646@163.com>
2019-06-06 17:41:45 +0800

    Initial commit

---
+++ src/main/java/com/cloud/bigdata/bdaggrateserverboot/BdAggrateserverBootApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.bigdata.bdaggrateserverboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdAggrateserverBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdAggrateserverBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cloud/bigdata/bdaggrateserverboot/BdAggrateserverBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloud.bigdata.bdaggrateserverboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BdAggrateserverBootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
